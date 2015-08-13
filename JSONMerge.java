3b5da884ff8c5e80026cb60b659fa323cd703f69
Andrew Zimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-08-12 21:47:30 -0700

    Update JSONMerge.java

--- JSONMerge.java
+++ JSONMerge.java
@@ -1,8 +1,8 @@
-//Requires org.json and org.json.simple libraries
-
 import java.io.*;
 import java.util.*;
 import java.io.FileReader;
+import java.net.URL;
+
 import org.json.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -12,15 +12,17 @@ public class JsonMerge
 {
 	public static File chosenOne;
 	public static JSONObject newobject;
+	public static String lines;
+	public static ArrayList<String> lined = new ArrayList();
 	@SuppressWarnings({ "unused" })
 	public static void main(String[] args) throws ParseException, FileNotFoundException
 	{
 		JSONParser parser = new JSONParser();
-		File f = new File("/Users/azimdars/documents/syria_instagram_json");
+		File f = new File("/Users/azimdars/documents/syria_instagram_json/instagram_json/");
 		ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
-		for (int i = 9359; i <= files.size()-1;i++)
+		for (int i = 0 ; i < files.size();i++)
 		{
-				File newFile = new File("/Users/azimdars/documents/syria_instagram_json/"+i+".json");
+				File newFile = new File("/Users/azimdars/Documents/syria_instagram_json/instagram_json/"+i+".json");
 				chosenOne = new File(""+newFile);
 				System.out.println(chosenOne);
 			try
@@ -38,8 +40,88 @@ public class JsonMerge
 					{
 						JSONObject theID = ids.getJSONObject(b);
 						String finall = theID.getString("id");
-						System.out.println("Image Number: " + finall);
-						File indexFile = new File(""+ finall);	
+						URL download = new URL("http://imagecat.dyndns.org:8081/solr/imagecatdev/select?q=id%3A*"+finall+".jpg*&wt=json&indent=true");
+						try (BufferedReader reader = new BufferedReader(new InputStreamReader(download.openStream(), "UTF-8"))) 
+						{
+						    for (String line; (line = reader.readLine()) != null;) 
+						    {
+						    	lines = line;
+						    	lined.add(lines);
+						    	System.out.println(lines);
+						    }
+						}
+						File indexJSONFile2 = new File("/Users/azimdars/Documents/syria_instagram_json/imagecatdev_solr_json/"+ finall+".img");
+						System.out.println(indexJSONFile2);
+						if (indexJSONFile2.exists())
+						{
+							indexJSONFile2.delete();
+						}
+						if(indexJSONFile2.createNewFile() == true)
+						{
+							System.out.println("File Has Been Downloaded!");
+							FileWriter inputter = new FileWriter(indexJSONFile2);
+							try 
+							{
+					            inputter.write(lined.toString());
+					            System.out.println("Successfully Copied JSON Object to File...");
+					        } 
+							catch (IOException e) 
+							{
+					            e.printStackTrace();
+					 
+					        } 
+							finally 
+							{
+					            inputter.flush();
+					            inputter.close();
+					 
+					        }
+						}
+						else
+						{
+							System.out.println("Error in File Download for: "+download);
+						}
+						File indexFile = new File("/Users/azimdars/Documents/syria_instagram_json/imagecatdev_solr_json/"+ finall+".img");
+						if (indexFile.isFile())
+						{
+							File newJSONFile = new File("/Users/azimdars/Documents/syria_instagram_json/finished_json/"+finall+".json");
+							if (newJSONFile.exists())
+							{
+								newJSONFile.delete();
+							}
+							if(newJSONFile.createNewFile() == true)
+							{
+								System.out.println("JSON Base Created!");
+							}
+							else
+							{
+								System.out.println("ERROR");
+							}
+							Object newObj = parser.parse(new FileReader(indexFile));
+							String newInfo = newObj.toString();
+							JSONArray FinalJSON = new JSONArray();
+							FinalJSON.put(newInfo);
+							FinalJSON.put(info);
+							FileWriter finalFile = new FileWriter(newJSONFile);
+							try 
+							{
+					            finalFile.write(FinalJSON.toString());
+					            System.out.println("Successfully Copied JSON Object to File...");
+					        } 
+							catch (IOException e) 
+							{
+					            e.printStackTrace();
+					 
+					        } 
+							finally 
+							{
+					            finalFile.flush();
+					            finalFile.close();
+					 
+					        }
+							
+						}
+						break;
 					}	
 				}
 			}
@@ -48,5 +130,6 @@ public class JsonMerge
 				System.out.println(e);
 			}
 		}
+		System.out.println("DONE");
 	}
 }
